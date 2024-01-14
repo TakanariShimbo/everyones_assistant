cb7cbf0763abcf9d6ac5c2bb71a6d5ccd9102157
TakanariShimbo <takanari.shimbo@gmail.com>
2024-01-14 17:15:10 +0900

    revise

--- app_server/model/static/path_list.py
+++ app_server/model/static/path_list.py
@@ -1,19 +1,20 @@
+import os
 
 
-BASE_DIR = "./model/static/"
-LOTTIE_DIR = BASE_DIR + "lotties/"
-TABLE_DIR = BASE_DIR + "tables/"
+BASE_DIR = os.path.dirname(__file__)
+LOTTIE_DIR = os.path.join(BASE_DIR, "lotties")
+TABLE_DIR = os.path.join(BASE_DIR, "tables")
 
 
 class LottiePathList:
-    loading = LOTTIE_DIR + "loading.json"
-    wake_up_logo = LOTTIE_DIR + "wake_up_logo_streamlit.json"
+    loading = os.path.join(LOTTIE_DIR, "loading.json")
+    wake_up_logo = os.path.join(LOTTIE_DIR, "wake_up_logo_streamlit.json")
 
 
 class TablePathList:
-    assistant = TABLE_DIR + "assistant.csv"
-    main_component = TABLE_DIR + "main_component.csv"
-    management_component = TABLE_DIR + "management_component.csv"
-    provider = TABLE_DIR + "provider.csv"
-    release = TABLE_DIR + "release.csv"
-    role = TABLE_DIR + "role.csv"
+    assistant = os.path.join(TABLE_DIR, "assistant.csv")
+    main_component = os.path.join(TABLE_DIR, "main_component.csv")
+    management_component = os.path.join(TABLE_DIR, "management_component.csv")
+    provider = os.path.join(TABLE_DIR, "provider.csv")
+    release = os.path.join(TABLE_DIR, "release.csv")
+    role = os.path.join(TABLE_DIR, "role.csv")
