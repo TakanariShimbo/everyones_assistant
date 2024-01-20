bbfcf94a521c7a80d83e30c0dbb2a424a7d8cdcd
TakanariShimbo <takanari.shimbo@gmail.com>
2024-01-20 12:54:20 +0900

    revise

--- app_server/app/base/__init__.py
+++ app_server/app/base/__init__.py
@@ -1,5 +1,4 @@
 from .page import BasePage
 from .component import BaseComponent
-from .pre_component import BasePreComponent
 from .processers import BaseProcesser, BaseProcessersManager, EarlyStopProcessException
 from .s_states import BaseSState
