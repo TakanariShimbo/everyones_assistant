b1baa214b1e5fdff82a97f1b2ec1d1103a55a640
TakanariShimbo <takanari.shimbo@gmail.com>
2024-01-21 09:45:00 +0900

    revise

--- app_server/app/main_app/s_states/sign_in_process/form.py
+++ app_server/app/main_app/s_states/sign_in_process/form.py
@@ -8,6 +8,6 @@ class Form(BaseModel):
     raw_password: str = Field(min_length=4)
 
     @classmethod
-    def init(cls, kwargs: Dict[str, Any]):
+    def init(cls, kwargs: Dict[str, Any]) -> "Form":
         required_dict = {name: kwargs[name] for name in cls.model_fields.keys()}
         return cls(**required_dict)
