9415e025e91978c0d42580de65be8aedbc5841f5
TakanariShimbo <takanari.shimbo@gmail.com>
2024-01-25 22:22:21 +0900

    revise

--- app_server/app/management_app/s_states/edit_account_table_process/processer.py
+++ app_server/app/management_app/s_states/edit_account_table_process/processer.py
@@ -10,8 +10,7 @@ class Processer(BaseProcesser[None]):
         edited_display_df = inner_dict["edited_display_df"]
 
         only_edited_table = account_table.get_only_edited_table(edited_display_df=edited_display_df)
-        for edited_account_entity in only_edited_table.get_all_beans():
-            edited_account_entity.update_record_of_database(database_engine=Database.ENGINE)
+        only_edited_table.update_records_of_database(database_engine=Database.ENGINE)
 
     def _pre_process(self, outer_dict: Dict[str, Any], inner_dict: Dict[str, Any]) -> None:
         pass
