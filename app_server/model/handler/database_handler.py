f3bf18e3dc271df758c00b6e071374fe6d1f37eb
TakanariShimbo <takanari.shimbo@gmail.com>
2024-01-27 20:59:33 +0900

    revise

--- app_server/model/handler/database_handler.py
+++ app_server/model/handler/database_handler.py
@@ -7,14 +7,21 @@ from sqlalchemy import Engine, text, CursorResult
 
 class DatabaseHandler:
     @staticmethod
-    def execute_sql(database_engine: Engine, statement: str, parameters: Optional[Dict[str, Any]]) -> CursorResult:
+    def execute_sql(
+        database_engine: Engine,
+        statement: str,
+        parameters: Optional[Dict[str, Any]],
+    ) -> CursorResult:
         with database_engine.connect() as conn:
             result = conn.execute(statement=text(statement), parameters=parameters)
             conn.commit()
         return result
 
     @staticmethod
-    def execute_sqls(database_engine: Engine, statement_and_parameters_list: List[Tuple[str, Optional[Dict[str, Any]]]]) -> List[CursorResult]:
+    def execute_sqls(
+        database_engine: Engine,
+        statement_and_parameters_list: List[Tuple[str, Optional[Dict[str, Any]]]],
+    ) -> List[CursorResult]:
         results = []
         with database_engine.connect() as conn:
             for statement, parameters in statement_and_parameters_list:
@@ -24,11 +31,21 @@ class DatabaseHandler:
         return results
 
     @staticmethod
-    def query_sql_on_pandas(database_engine: Engine, statement: str, parameters: Optional[Dict[str, Any]] = None, dtype_dict: Optional[Dict[str, Any]] = None) -> pd.DataFrame:
+    def query_sql_on_pandas(
+        database_engine: Engine,
+        statement: str,
+        parameters: Optional[Dict[str, Any]] = None,
+        dtype_dict: Optional[Dict[str, Any]] = None,
+    ) -> pd.DataFrame:
         return pd.read_sql_query(sql=text(statement), con=database_engine, params=parameters, dtype=dtype_dict)
 
     @staticmethod
-    def get_select_sql(table_name: str, filter_conditions: List[str], order_condition: Optional[str] = None, limit: Optional[int] = None) -> Tuple[str, None]:
+    def get_select_sql(
+        table_name: str,
+        filter_conditions: List[str],
+        order_condition: Optional[str] = None,
+        limit: Optional[int] = None,
+    ) -> Tuple[str, None]:
         condition_str = " AND ".join(filter_conditions)
         statement = f"SELECT * FROM {table_name} WHERE {condition_str}"
         if order_condition:
@@ -38,14 +55,25 @@ class DatabaseHandler:
         return statement, None
 
     @classmethod
-    def get_simple_select_sql(cls, table_name: str, column_name: str, value: Any, order_condition: Optional[str] = None, limit: Optional[int] = None) -> Tuple[str, Dict[str, Any]]:
+    def get_simple_select_sql(
+        cls,
+        table_name: str,
+        column_name: str,
+        value: Any,
+        order_condition: Optional[str] = None,
+        limit: Optional[int] = None,
+    ) -> Tuple[str, Dict[str, Any]]:
         filter_condition = f"{column_name} = :{column_name}"
         statement, _ = cls.get_select_sql(table_name=table_name, filter_conditions=[filter_condition], order_condition=order_condition, limit=limit)
         parameters = {column_name: value}
         return statement, parameters
 
     @staticmethod
-    def get_insert_sql(table_name: str, insert_column_names: List[str], return_column_names: List[str]) -> Tuple[str, None]:
+    def get_insert_sql(
+        table_name: str,
+        insert_column_names: List[str],
+        return_column_names: List[str],
+    ) -> Tuple[str, None]:
         insert_values = [f":{name}" for name in insert_column_names]
         insert_names_str = ", ".join(insert_column_names)
         return_names_str = ", ".join(return_column_names)
@@ -61,7 +89,11 @@ class DatabaseHandler:
         return statement, None
 
     @staticmethod
-    def get_update_sql(table_name: str, key_column_name: str, non_key_column_names: List[str]) -> Tuple[str, None]:
+    def get_update_sql(
+        table_name: str,
+        key_column_name: str,
+        non_key_column_names: List[str],
+    ) -> Tuple[str, None]:
         key_name_and_value_str = f"{key_column_name} = :{key_column_name}"
         non_key_name_and_values = [f"{name} = :{name}" for name in non_key_column_names]
         name_and_values_str = ", ".join(non_key_name_and_values)
@@ -76,7 +108,11 @@ class DatabaseHandler:
         return statement, None
 
     @staticmethod
-    def get_delete_sql(table_name: str, key_column_name: str, key_value: Any) -> Tuple[str, Dict[str, Any]]:
+    def get_delete_sql(
+        table_name: str,
+        key_column_name: str,
+        key_value: Any,
+    ) -> Tuple[str, Dict[str, Any]]:
         condition_str = f"{key_column_name} = :{key_column_name}"
 
         statement = dedent(
@@ -89,14 +125,19 @@ class DatabaseHandler:
         return statement, parameters
 
     @staticmethod
-    def get_upsert_sql(table_name: str, key_column_name: str, non_key_column_names: List[str], record_dicts: List[Dict[str, Any]]) -> Tuple[str, Dict[str, Any]]:
+    def get_upsert_sql(
+        table_name: str,
+        key_column_name: str,
+        non_key_column_names: List[str],
+        record_dicts: List[Dict[str, Any]],
+    ) -> Tuple[str, Dict[str, Any]]:
         column_names = [key_column_name] + non_key_column_names
         column_names_str = ", ".join(column_names)
-        
+
         n_record = len(record_dicts)
         record_str_list = []
         for i in range(n_record):
-            record_str = ', '.join([f":{column}_{i}" for column in column_names])
+            record_str = ", ".join([f":{column}_{i}" for column in column_names])
             record_str_list.append(f"({record_str})")
         records_str = ", ".join(record_str_list)
 
